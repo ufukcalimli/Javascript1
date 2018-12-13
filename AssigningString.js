9563e3c21d225ac6bfe7d37359f7afc130fc35de
ufukcalimli <ufk.calimli@gmail.com>
2018-12-13 14:44:38 +0100

    Delete AssigningString.js

--- AssigningString.js
+++
@@ -1,7 +0,0 @@
-let y="Sweetie";
-console.log("the value of my string will be: Sweetie");
-console.log(y);
-
-y="Horrible";
-console.log("the value of my string will be: Horrible");
-console.log(y);
